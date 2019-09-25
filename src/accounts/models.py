483c0fd0f0725388264deabedc6336f82828459a
konzamir <konzamir@gmail.com>
2019-09-25 20:06:54 +0300

    add unique property to email field

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,1 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class UserModel(AbstractUser):
+    email = models.EmailField(_('email address'), blank=False, unique=True)
--- src/accounts/serializers.py
+++ src/accounts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from accounts.models import UserModel as User
 from django.contrib.auth import authenticate
 
 
